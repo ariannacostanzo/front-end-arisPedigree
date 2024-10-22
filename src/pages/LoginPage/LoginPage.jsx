de706b79b0e92c3ed64a9b4a32f0db20d36dd905
ariannacostanzo <costanzo.arianna@outlook.it>
2024-10-22 16:00:26 +0200

    creating register and login page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,8 +1,68 @@
+import { useState } from "react";
+import { useAuth } from "../../providers/authProvider.jsx";
+import { useNavigate } from "react-router-dom"; 
+
 const LoginPage = () => {
-     return (
-     <>
-       <div>login</div>
-     </>
-     )
-}
+
+  const navigate = useNavigate();
+  const { login } = useAuth();
+  const [formData, setFormData] = useState({
+    emailOrUsername: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const { emailOrUsername, password } = formData;
+
+    try {
+      const payload = { emailOrUsername, password };
+      await login(payload);
+      navigate("/userDetail");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <div>login</div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="emailOrUsername">User name or Email:</label>
+          <input
+            type="emailOrUsername"
+            name="emailOrUsername"
+            value={formData.emailOrUsername}
+            onChange={handleChange}
+            autoComplete="emailOrUsername"
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            autoComplete="password"
+            required
+          />
+        </div>
+
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
+};
 export default LoginPage;
--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,8 +1,109 @@
+import { useState } from "react";
+import axios from "../../utils/axiosClient.js";
+import { useAuth } from "../../providers/authProvider.jsx";
+import { useNavigate } from "react-router-dom";
+
 const RegisterPage = () => {
-     return (
-     <>
-       <div>register page</div>
-     </>
-     )
-}
+  const navigate = useNavigate();
+  const { setIsLoggedIn } = useAuth();
+  const [formData, setFormData] = useState({
+    email: "",
+    name: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const { email, name, password, confirmPassword } = formData;
+
+    if (password !== confirmPassword) {
+      throw new Error("Passwords do not match");
+    }
+
+    try {
+      const response = await axios.post(
+        "/auth/register",
+        {
+          email,
+          name,
+          password,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      console.log(response);
+
+      console.log("Registration successful!");
+      setIsLoggedIn(true);
+      navigate("/userDetail");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <>
+      <div>register page</div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            autoComplete="email"
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="name">Username:</label>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            autoComplete="name"
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            autoComplete="password"
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="confirmPassword">Confirm Password:</label>
+          <input
+            type="password"
+            name="confirmPassword"
+            value={formData.confirmPassword}
+            onChange={handleChange}
+            autoComplete="confirmPassword"
+            required
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </>
+  );
+};
 export default RegisterPage;
