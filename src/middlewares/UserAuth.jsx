a8913d9ee7b37b4e42de9b35826d7f80b08efb1f
DaniAm94 <dani.amico94@gmail.com>
2024-12-20 17:21:43 +0100

    removed console.log from user auth

--- src/middlewares/UserAuth.jsx
+++ src/middlewares/UserAuth.jsx
@@ -4,7 +4,6 @@ import { Navigate, useLocation, Outlet } from "react-router-dom";
 const UserAuth = () => {
     const { isLoggedIn } = useAuth();
     const location = useLocation();
-    console.log(location);
     if (!isLoggedIn) {
         return <Navigate to="/login" state={{ errorMessage: 'You must be logged in!', redirectTo: location.pathname }} />
     }
