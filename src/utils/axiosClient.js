cce20cfbbdeedba341a86afba869fbddef8218d6
ariannacostanzo <costanzo.arianna@outlook.it>
2025-02-09 18:10:23 +0100

    updated axios timeout

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: "https://back-end-arispedigree-aq4t.onrender.com",
-  timeout: 10000,
+  timeout: 1000000,
 });
 
 export default instance;
